10c971190fa2179943bac17213203d810bb85f75
Jean-Lionel <nijeanlionel@gmail.com>
2023-07-14 12:32:18 +0200

    initial commit

---
+++ src/components/bord/Board.vue
@@ -0,0 +1,29 @@
+<template>
+    <div 
+    :id="id"
+    @dragover.prevent
+    @drop.prevent="drop"
+    >
+        <slot/>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["id"],
+    methods: {
+        drop: e => {
+            console.log(e)
+            const card_id = e.datatransfer.getData('card_id');
+            const card = document.getElementById(card_id);
+            card.style.display = 'block';
+            e.target.appedChild(card);
+        }
+    }
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+       <div class="container-fluid">
+        <div class="row">
+            <div class="col-2">
+                <LeftSide/>
+            </div>
+            <div class="col-8">
+                <board id="bord-1">
+                    <card
+                    id="card-1"
+                    :draggable="true"
+                    >
+                    <h1>ello </h1>
+                    </card>
+                </board>
+                <board id="bord-2">
+                    <card
+                    id="card-2"
+                    :draggable="true"
+                    >
+                    <h1>Card 3 </h1>
+                    </card>
+                </board>
+            </div>
+            <div class="col-2"></div>
+        </div>
+        
+       </div>
+    </div>
+</template>
+
+<script>
+import LeftSide from '@/components/side/LeftSide.vue'
+import Board from '@/components/bord/Board.vue'
+import Card from '@/components/bord/Card.vue'
+    export default {
+  components: { LeftSide, Board, Card },
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
